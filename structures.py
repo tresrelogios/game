44e4d415fc9284c49ecabd02e29b82f63ba37eb6
RojerGS <rodrigomserrao@gmail.com>
2015-06-30 17:38:30 +0100

    lel 2.1

--- structures.py
+++ structures.py
@@ -1,4 +1,4 @@
-from global_vars import *
+from GLOBS import *
 
 class Building(object):
     """Building class"""
@@ -19,16 +19,31 @@ class Building(object):
             raise TypeError("Building level should be of type int")
     
         if type(cost) == dict:
+            if len(cost) != len(RESOURCES):
+                raise ValueError("Cost dict of wrong size", len(cost),
+                                "should be", len(RESOURCES))
             for r in RESOURCES:
-                pass
+                val = cost.get(r, None)
+                if type(val) != int:
+                    if val is None:
+                        raise KeyError(r, "cost missing")
+                    else:
+                        raise TypeError("Cost value should be an int")
+                elif val < 0:
+                    raise ValueError("Cost value should not be negative")
+            self.cost = cost
         else:
             raise TypeError("Resource costs should be a dictionary")
-        self.increment = increment
+
+        if type(increment) == float and 1 <= increment <= 2:
+            self.increment = increment
+        else:
+            if type(increment) != float:
+                raise TypeError("Increment value should be a float")
+            else:
+                raise ValueError("Increment value should be bounded by 1 and 2")
 
     def level_up(self):
         self.level += 1
         for resource in self.cost:
             self.cost[resource] = int(self.cost[resource]*increment)
-
-def outronome():
-    return "fuck you"
