2cd604e7a6be8f9f81e3317981928d9e471d9e60
João Pinheiro <joao.castropinheiro@gmail.com>
2015-06-30 15:56:47 +0100

    flask initial test

---
+++ index.py
@@ -0,0 +1,13 @@
+from Flask import render_template, Flask
+
+app = Flask(__name__)
+
+
+def helloworld():
+	return 'hello world'
+
+
+@app.route('/')
+def index():
+	text = helloworld()
+	return render_template('index.html', text=text)
