4c4e815ca4858f27c3728506282ed36d240fc441
RojerGS <rodrigomserrao@gmail.com>
2015-06-30 16:41:00 +0100

    lel 2

--- building.py
+++ building.py
@@ -1,11 +1,28 @@
+from global_vars import *
 
 class Building(object):
     """Building class"""
     def __init__(self, name, level, cost, increment):
         """Initializes the building; Does type checking"""
-        self.name = name
-        self.level = level
-        self.cost = cost
+
+        if type(name) == str and name:
+            self.name = name
+        else:
+            raise TypeError("Building name should be a non-empty string")
+
+        if type(level) == int:
+            if level > -1:
+                self.level = level
+            else:
+                raise ValueError("Building level should be a non-negative int")
+        else:
+            raise TypeError("Building level should be of type int")
+    
+        if type(cost) == dict:
+            for r in RESOURCES:
+                pass
+        else:
+            raise TypeError("Resource costs should be a dictionary")
         self.increment = increment
 
     def level_up(self):
